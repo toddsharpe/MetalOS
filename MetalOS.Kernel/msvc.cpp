4e6903f7b0643e8f803ceb0a2171f4c2efac4cf7
Todd Sharpe <toddsharpe@outlook.com>
2019-12-28 22:50:25 -0800

    Added msvc files

---
+++ MetalOS.Kernel/msvc.cpp
@@ -0,0 +1,41 @@
+#include "msvc.h"
+#include "Main.h"
+#include "Kernel.h"
+
+namespace std
+{
+	void __cdecl _Xbad_alloc()
+	{
+		Assert(false);
+	}
+	void __cdecl _Xinvalid_argument(const char*)
+	{
+		Assert(false);
+	}
+	void __cdecl _Xlength_error(const char*)
+	{
+		Assert(false);
+	}
+	void __cdecl _Xout_of_range(const char*)
+	{
+		Assert(false);
+	}
+	void __cdecl _Xoverflow_error(const char*)
+	{
+		Assert(false);
+	}
+	void __cdecl _Xruntime_error(const char*)
+	{
+		Assert(false);
+	}
+}
+
+void __cdecl _invalid_parameter(char const*, char const*, char const*, unsigned int, int)
+{
+
+}
+
+extern "C" void __chkstk()
+{
+
+}
